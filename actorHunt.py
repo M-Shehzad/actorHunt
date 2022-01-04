3441db5d5c9717180713970376a1e436ca97f305
m-shehzad <shehzadboii@gmail.com>
2022-01-04 16:58:58 +0530

    response received in frontend console

--- actorHunt.py
+++ actorHunt.py
@@ -1,32 +1,50 @@
-from flask import Flask, json, redirect, url_for, render_template, request, jsonify, make_response
+from flask import (
+    Flask,
+    json,
+    redirect,
+    url_for,
+    render_template,
+    request,
+    jsonify,
+    make_response,
+)
 import writingInfo
 import requests
 
 app = Flask(__name__)
 
-@app.route("/", methods = ["POST", "GET"])
+
+@app.route("/", methods=["POST", "GET"])
 def mainPage():
     return render_template("index.html", content="")
 
     # actorName, actorJobs, actorBD, actorPhotoPath = writingInfo.DoIt(request.form["actor"])
     # return render_template("actorDisplay.html", content=[actorName, actorJobs, actorBD, actorPhotoPath])
     # print(request.form["actor"])
-    
 
-@app.route("/search", methods = ["POST", "GET"])
+
+@app.route("/search", methods=["POST", "GET"])
 def getJson():
     if request.method == "POST":
         # print(request)
         req = request.get_json()
         print(req)
         actorName, actorJobs, actorBD, actorPhotoPath = writingInfo.DoIt(req["name"])
-        res = make_response(jsonify({"name":actorName,
-                                        "job": actorJobs,
-                                        "dob":actorBD,
-                                        "pic":actorPhotoPath}), 200)
+        res = make_response(
+            jsonify(
+                {
+                    "name": actorName,
+                    "job": actorJobs,
+                    "dob": actorBD,
+                    "pic": actorPhotoPath,
+                }
+            ),
+            200,
+        )
         print(res.data)
-        return "post request working"
+        return res
     return "get request"
+
 
 @app.route("/result", methods=["POST"])
 def resultPage(par):
